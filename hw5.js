9e8dc95400256e05f98d2aad4c4e0db107199821
nandityasf <you@example.com>
2023-09-11 02:28:05 +0700

    js file to tabform hw5

--- hw5.js
+++ hw5.js
@@ -1,23 +1,78 @@
-const regisForm = document.getElementById("regisForm")
-const tabPendaftar = document.getElementById("tabPendaftar")
-const resume = document.getElementById("resume")
-if (nama.length < 10 || umur < 25 || uang < 100 || uang > 1000000) {
-        alert("Pastikan data yang Anda masukkan sesuai kriteria!");
-        return;
+function openTab(tabName) {
+    var tabs = document.getElementsByClassName('tab-content');
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].style.display = 'none';
     }
-    pendaftar.push({ nama, umur, uang });
-
-let pendaftar = []
-
-function submitForm(){
-    const name = document.getElementById("name").value
-    const age = parseInt(document.getElementById("age").value)
-    const money = parseInt(document.getElementById("money").value)
-
-    if (nama.length < 10 || umur < 25 || uang < 100 || uang > 1000000) {
-        alert("Pastikan data yang Anda masukkan sesuai kriteria!");
-        return;
-    }
-    pendaftar.push({ name, age, money });
-    updateTable();
+    document.getElementById(tabName).style.display = 'block';
 }
+//OOP
+class Pendaftar {
+    constructor(nama, umur, uangSaku) {
+      this.nama = nama;
+      this.umur = umur;
+      this.uangSaku = uangSaku;
+    }
+  }
+
+  const pendaftarList = [];
+
+  function validateForm() {
+    const nama = document.getElementById('nama').value;
+    const umur = parseInt(document.getElementById('umur').value);
+    const uangSaku = parseInt(document.getElementById('uangSaku').value);
+
+    if (nama.length < 10 || umur < 25 || uangSaku < 100000 || uangSaku > 100000000) {
+      alert('mohon isi form dengan sesuai');
+      return;
+    }
+
+    const pendaftar = new Pendaftar(nama, umur, uangSaku);
+    pendaftarList.push(pendaftar);
+
+    document.getElementById('nama').value = '';
+    document.getElementById('umur').value = '';
+    document.getElementById('uangSaku').value = '';
+
+    updateListAndResume();
+  }
+
+  function updateListAndResume() {
+    const pendaftarTable = document.getElementById('pendaftarTable');
+    const pendaftarData = document.getElementById('pendaftarData');
+    const averageUmur = document.getElementById('averageUmur');
+    const averageUangSaku = document.getElementById('averageUangSaku');
+
+    pendaftarData.innerHTML = '';
+
+    let totalUmur = 0;
+    let totalUangSaku = 0;
+
+    for (const pendaftar of pendaftarList) {
+      const row = document.createElement('tr');
+      row.innerHTML = `<td>${pendaftar.nama}</td><td>${pendaftar.umur}</td><td>${pendaftar.uangSaku}</td>`;
+      pendaftarData.appendChild(row);
+
+      totalUmur += pendaftar.umur;
+      totalUangSaku += pendaftar.uangSaku;
+    }
+
+    const averageUmurValue = pendaftarList.length > 0 ? totalUmur / pendaftarList.length : 0;
+    const averageUangSakuValue = pendaftarList.length > 0 ? totalUangSaku / pendaftarList.length : 0;
+
+    averageUmur.textContent = averageUmurValue.toFixed(0);
+    averageUangSaku.textContent = averageUangSakuValue.toFixed(0);
+  }
+
+  // tab aktif
+  const tabs = document.querySelectorAll('.tabs li a');
+  tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+      tabs.forEach(t => t.classList.remove('active'));
+      tab.classList.add('active');
+
+      const tabContentId = tab.getAttribute('href').replace('#', '');
+      const tabContents = document.querySelectorAll('.tab-content');
+      tabContents.forEach(content => content.classList.add('hidden'));
+      document.getElementById(tabContentId).classList.remove('hidden');
+    });
+  });
